1c37f7c647913174d753738b612a8cee5beb3694
gzero1016 <gzero1016@gmail.com>
2023-11-02 16:23:03 +0900

    Docs: 파일위치수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AccountRoute from './components/Routes/AccountRoute';
+import SignUp from './pages/SignUp/SignUp';
 
 function App() {
   return (
     <Routes>
       <Route path='/account/*' element={ <AccountRoute /> } />
-      <Route path='/signup' element=<SignUp/> />
+      <Route path='/signup' element={ <SignUp/> } />
     </Routes>
   );
 }
