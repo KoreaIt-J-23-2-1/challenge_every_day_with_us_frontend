486f343a351dd7c45acc3c2ee958380e35df0553
comet7406 <jsp7406@naver.com>
2023-11-06 14:27:44 +0900

    Feat: challenge조회기능 구현

--- src/components/BaseLayout/BaseLayout.jsx
+++ src/components/BaseLayout/BaseLayout.jsx
@@ -5,8 +5,6 @@ import { css } from '@emotion/react';
 const Layout = css`
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
     margin: 50px;
 `;
 
