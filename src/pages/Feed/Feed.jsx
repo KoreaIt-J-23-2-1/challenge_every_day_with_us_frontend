138807152602ef243453cd47df9f4d3cd239560c
younghk37 <younghk37@gmail.com>
2023-11-14 09:39:24 +0900

    Feat: 챌린지 피드 댓글 작성-#137

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -10,6 +10,7 @@ function Feed(props) {
     const [ feedList, setFeedList ] = useState([]);
     const [ page, setPage ] = useState(1);
     const lastChallengeRef = useRef();
+    const [ commentInputList, setCommentInputList ] = useState();
     
     useEffect(() => {
         const observerService = (entries, observer) => {
@@ -52,6 +53,23 @@ function Feed(props) {
         await instance.post("/api/challenge/report", data, option)
     }
 
+    const handleCommentInput = (e) => {
+        setCommentInputList({
+            ...commentInputList,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const handleCommnetSubmit = async (feedId) => {
+        try {
+            await instance.post(`/api/feed/${feedId}/comment`, {commentContent: commentInputList[`commentInput${feedId}`]}, option);
+            alert("댓글 등록 성공! -> " + feedId + "피드");
+            console.log(commentInputList[`commentInput${feedId}`]);
+        }catch(error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div css={S.SLayout}>
             <div css={S.SHeaderLayout}>
@@ -78,9 +96,6 @@ function Feed(props) {
                                     <p>[{feed.categoryName}]</p>
                                     <div><b>{feed.challengeName}</b> Challenge</div>
                                 </div>
-                                    {feed.stopWatch !== 0 ? (
-                                        <div>{convertSecondsToTime(feed.stopWatch)}</div>
-                                    ) : (null)}
                                 <img src={feed.img} alt="" />
                             </div>
                             <div css={S.SText}>
@@ -92,12 +107,12 @@ function Feed(props) {
 
                         <div css={S.SFeedBottomLayout}>
                             <div css={S.SFeedBottomHeader}>
-                                <b>좋아요</b>
-                                <b>댓글</b>
+                                <button>좋아요</button>
+                                <button>댓글</button>
                             </div>
                             <div css={S.SFeedBottomBody}>
                                 <div>이미지</div>
-                                <div><p>{principalState.data.data.nickname}</p>댓글</div>
+                                <div><p>{principalState.data.data.nickname}</p><input type="text" name={`commentInput${feed.feedId}`} onChange={handleCommentInput}/><button onClick={() => {handleCommnetSubmit(feed.feedId)}}>댓글달기</button></div>
                             </div>
                         </div>
                         </div>
@@ -128,13 +143,4 @@ function getTimeDifference(feedDateTime) {
         const days = Math.floor(timeDifferenceInSeconds / 86400);
         return `${days}일 전`;
     }
-}
-
-function convertSecondsToTime(seconds) {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-
-    const formattedTime = `${hours > 0 ? hours + '시간 ' : ''}${minutes > 0 ? minutes + '분 ' : ''}${remainingSeconds}초`;
-    return formattedTime.trim();
 }
