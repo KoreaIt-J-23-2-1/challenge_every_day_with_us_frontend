498f6c22510327b00d94ca0a1d5593b993db5588
young37 <younghk37@gmail.com>
2023-11-16 23:31:54 +0900

    Feat: 비로그인 상태에서 버튼 막기-#180

--- src/components/MenuModal/MenuModal.jsx
+++ src/components/MenuModal/MenuModal.jsx
@@ -2,35 +2,44 @@ import React from 'react';
 /** @jsxImportSource @emotion/react */
 import * as S from './MenuModalStyle';
 import { useNavigate } from 'react-router-dom';
-
-
+import { useQueryClient } from 'react-query';
 
 function MenuModal(props) {
     const navigate = useNavigate(); 
+    const queryClient = useQueryClient();
+    const principalState = queryClient.getQueryState("getPrincipal");
+    const principal = principalState.data;
 
-    
+    const checkLoginBeforeNavigate = (path) => {
+        if(!principal) {
+            alert("로그인을 먼저 진행해주세요");
+            return;
+        }
+        navigate(path);
+    };
+
     return (
         <div css={S.Background}>
             <div css={S.MenuHeader}>
                 <h3></h3>
                 <div css={S.MyBox}>
                     <button onClick={() => { navigate("/auth/signin") }}>로그인</button>
-                    <button onClick={() => { navigate("/account/mypage") }}>마이페이지</button>
-                    <button onClick={() => { navigate("/account/mypage/detail") }}>내정보수정</button>
+                    <button onClick={() => { checkLoginBeforeNavigate("/account/mypage") }}>마이페이지</button>
+                    <button onClick={() => { checkLoginBeforeNavigate("/account/mypage/detail") }}>내정보수정</button>
                 </div>
             </div>
 
             <div>
                 <div css={S.BtnBox}>
-                    <button onClick={() => { navigate("/notice/page/1") }}>공지</button>
-                    <button onClick={() => { navigate("/store/items") }}>상점</button>
+                    <button onClick={() => { checkLoginBeforeNavigate("/notice/page/1") }}>공지</button>
+                    <button onClick={() => { checkLoginBeforeNavigate("/store/items") }}>상점</button>
 
 
                 </div>
                 <div css={S.BtnBox}>
-                    <button onClick={() => { navigate("/challenge/category") }}>챌린지카테고리</button>
-                    <button onClick={() => { navigate("/challenges") }}>챌린지리스트조회</button>
-                    <button onClick={() => { navigate("/challenge/feed") }}>Feed</button>
+                    <button onClick={() => { checkLoginBeforeNavigate("/challenge/category") }}>챌린지카테고리</button>
+                    <button onClick={() => { checkLoginBeforeNavigate("/challenges") }}>챌린지리스트조회</button>
+                    <button onClick={() => { checkLoginBeforeNavigate("/challenge/feed") }}>Feed</button>
                 </div>
                 <div css={S.BtnBox}>
                     <button>룰루랄라</button>
