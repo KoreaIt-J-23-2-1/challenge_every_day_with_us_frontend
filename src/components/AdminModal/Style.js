c17823ceee0f6fce3500bceec033851c12a6d645
gzero1016 <gzero1016@gmail.com>
2023-11-16 23:23:36 +0900

    Feat:chart 데이터받아오기 완료 정렬 Ing

--- src/components/AdminModal/Style.js
+++ src/components/AdminModal/Style.js
@@ -1,9 +1,12 @@
 import { css } from '@emotion/react';
 
 export const SubModal = css`
+    height: auto;
+    width: auto;
     border-top: 2px solid #dbdbdb;
 
     & li {
+        width: 100px;
         margin-bottom: 9px;
     }
 `;
