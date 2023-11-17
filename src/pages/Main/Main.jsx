dbebd8bad2cf91f5ff12b0e601aadbdfc22a5062
geunhaemoon <nevve1@duam.net>
2023-11-17 10:13:14 +0900

    Fix: 수정

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -57,11 +57,11 @@ function Main(props) {
                     
                     {principal ? <button onClick={handleLogoutButton}>로그아웃</button>
                     : <button onClick={() => { navigate("/auth/signin") }}>로그인</button>}
-                    <button onClick={() => { checkLoginBeforeNavigate("/store/items") }}>상점</button>
+                    {/* <button onClick={() => { checkLoginBeforeNavigate("/store/items") }}>상점</button> */}
                     <button onClick={() => { checkLoginBeforeNavigate(`/store/${userId}/orders`) }}>상점 물품 구매 목록 조회</button>
                     <button onClick={() => { checkLoginBeforeNavigate("/account/mypage") }}>마이페이지</button>
                     <button onClick={() => { checkLoginBeforeNavigate("/account/mypage/detail") }}>내정보수정</button>
-                    <button onClick={() => { checkLoginBeforeNavigate("/point") }}>포인트상점</button>
+                    {/* <button onClick={() => { checkLoginBeforeNavigate("/point") }}>포인트상점</button> */}
                     <button onClick={() => { checkLoginBeforeNavigate("/notice/page/1") }}>공지목록</button>
                     <button onClick={() => { checkLoginBeforeNavigate("/challenge/category") }}>챌린지 생성 </button>
                     {/* <button onClick={() => { checkLoginBeforeNavigate("/challenge/5") }}>챌린지조회</button> */}
