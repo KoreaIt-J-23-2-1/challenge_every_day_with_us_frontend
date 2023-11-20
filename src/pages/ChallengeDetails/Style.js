d96039d43199295e2b3c9aee35bf76402afd2e6d
geunhaemoon <nevve1@duam.net>
2023-11-20 14:12:57 +0900

    Fix: 구성 수정

--- src/pages/ChallengeDetails/Style.js
+++ src/pages/ChallengeDetails/Style.js
@@ -3,30 +3,24 @@ import { css } from '@emotion/react';
 export const Layout = css`
     display: flex;
     flex-direction: column;
+    width: 90%;
+    height: 100%;
 `;
 
 export const HeaderLayout = css`
     display: flex;
-    justify-content: space-between;
+    /* justify-content: space-between; */
     align-items: flex-end;
-    height: 150px;
-    margin: 0px 30px;
+    height: 120px;
+    width: 100%;
+    padding-bottom: 10px;
+    border-bottom: 4px solid #999;
 
-    & b {
-        font-size: 30px;
-    }
-
-    & p {
-        margin: 0px;
-        margin-top: 20px;
-    }
 `;
 
 export const Box = css`
     display: flex;
-    justify-content: center;
     align-items: center;
-
 `;
 
 export const DeleteButton = css`
@@ -53,62 +47,38 @@ export const Writer = css`
 
 export const BodyLayout = css`
     display: flex;
-    justify-content: space-between;
-    margin: 0px 30px;
-    
-    & img {
-        
-    }
+    margin: 20px 0px;
+    /* justify-content: center; */
+    width: 100%;
+    gap: 30px;
 `;
 
-export const BodyFeedLayout = css`
-    display: flex;
-    flex-grow: 1;
-    height: 700px;
-    border: 2px solid #dbdbdb;
-    border-radius: 10px;
-    overflow: hidden;
-    overflow-y: auto;
-
-    ::-webkit-scrollbar {
-    width: 2px;
-    }
-
-    ::-webkit-scrollbar-thumb {
-    background-color: #dbdbdb
-    }
-`;
 
 export const BodyRightBox = css`
     display: flex;
     flex-direction: column;
-    margin-left: 20px;
+    min-width: 400px;
 `;
 
 export const ParticipationButton = css`
-    margin-top: 20px;
-    width: 500px;
+    width: 100%;
     height: 40px;
     background-color: transparent;
     border: 1px solid #dbdbdb;
     border-radius: 10px;
     cursor: pointer;
-
     &:active {
         background-color: #eee;
     }
 `;
 
 export const textBox = css`
-    width: 500px;
     height: 100px;
     border: 2px solid #dbdbdb;
-    margin: 20px 0px;
 `;
 
 export const SLikeButton = (isLike) => css`
     position: sticky;
-    margin: 0px 40px;
     border: 1px solid #dbdbdb;
     border-radius: 50%;
     width: 40px;
@@ -117,16 +87,10 @@ export const SLikeButton = (isLike) => css`
     cursor: pointer;
 `;
 
-export const line = css`
-    margin: 10px 20px;
-    border-bottom: 2px solid #dbdbdb;
-`;
 
 export const ListBox = css`
     display: flex;
     flex-direction: column;
-    margin-top: 20px;
-    height: 445px;
     overflow: auto;
     border: 1px solid #dbdbdb;
     border-radius: 5px;
@@ -134,27 +98,11 @@ export const ListBox = css`
     & scrollable-container {
         height: 100%;
     }
-
-    & b {
-        margin: 5px;
-    }
-
-    & p {
-        margin: 5px;
-    }
 `;
 
 export const ListContainer = css`
     display: flex;
     align-items: center;
-
-    & p {
-        margin: 5px;
-    }
-
-    & button {
-        
-    }
 `;
 
 export const DeleteChallengerButton = css`
@@ -170,57 +118,65 @@ export const DeleteChallengerButton = css`
     }
 `;
 
+// 왼쪽 피드 
+export const BodyFeedLayout = css`
+    display: flex;
+    flex-direction: column;
+    width: 700px;
+    height: 700px;
+    border-radius: 10px;
+    overflow: hidden;
+    overflow-y: auto;
+    background: rgba(255, 255, 255, 0.7); 
+    border-radius: 15px; 
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    ::-webkit-scrollbar {
+    width: 2px;
+    }
+    ::-webkit-scrollbar-thumb {
+    background-color: #dbdbdb
+    }
+`;
+
 export const SLayout = css`
     display: flex;
     flex-direction: column;
-    margin: 30px 70px;
-    width: 100%;
+    /* justify-content: center;   */
+    width: 560px;
+    margin: 20px auto;
 `;
 
-export const SFeedContainer = css`
-    flex-direction: column;
-    border: 1px solid #dbdbdb;
-    border-radius: 10px;
-    width: 100%;
-`;
 
 export const SFeedLayout = css`
     display: flex;
     flex-direction: column;
+    align-items: center;
+
+    margin: 0px auto;
 `;
 
 export const SFeedHeader = css`
     display: flex;
-    justify-content: space-between;
+    /* justify-content: space-between; */
     align-items: center;
 
-    & div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+`;
 
-    & button {
-        margin: 0px 20px;
-    }
-
-    & img {
-        margin: 20px;
-        width: 40px;
-        height: 40px;
-        border: 1px solid #dbdbdb;
-        border-radius: 50%;
-    }
+export const FeedImg = css`
+    width: 40px;
+    height: 40px;
+    border: 1px solid #dbdbdb;
+    border-radius: 50%;
 `;
 
 export const SFeedBody = css`
     display: flex;
-    justify-content: space-between;
+    /* justify-content: space-between; */
     margin-left: 20px;
 
     & img {
-        max-width: 450px;
-        height: 400px;
+        max-width: 150px;
+        max-height: 150px;
         margin-right: 20px;
         border-radius: 10px;
     }
@@ -251,14 +207,14 @@ export const SText = css`
 export const SInfo = css`
     display: flex;
     justify-content: flex-end;
-    align-items: center;
+    /* align-items: center; */
     margin: 10px 20px;
 `;
 
 export const SFeedBottomLayout = css`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    /* justify-content: center; */
     align-items: center;
     width: 100%;
     border-top: 1px solid #dbdbdb;
@@ -266,12 +222,10 @@ export const SFeedBottomLayout = css`
 
 export const SFeedBottomHeader = css`
     display: flex;
-    justify-content: space-between;
     align-items: center;
 `;
 
 export const SFeedBottomBody = css`
     display: flex;
-    justify-content: center;
     align-items: center;
 `;
