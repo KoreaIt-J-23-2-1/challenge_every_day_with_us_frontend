470efd782cc8b1506f937cfcd9608654c4100d99
gzero1016 <gzero1016@gmail.com>
2023-11-22 00:35:10 +0900

    Feat:Google 로그인

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SignLayout from '../../components/SignLayout/SignLayout';
 import { css } from '@emotion/react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
@@ -6,20 +6,22 @@ import { async } from 'q';
 import { instance } from '../../api/config/instance';
 /** @jsxImportSource @emotion/react */
 import * as S from './Style';
+import { storage } from '../../api/firebase/firebase';
+import { getDownloadURL, ref, uploadBytes } from '@firebase/storage';
 
 function SignUp(props) {
     const navigete = useNavigate();
     const [ searchParams, setSearchParams ] = useSearchParams();
 
     const user = {
-        email: "",
+        email: searchParams.get('email'),
         password: "",
-        name: "",
+        name: searchParams.get('name'),
         nickname: "",
         phone:"",
         oauth2Id: searchParams.get("oauth2Id"),
-        profileUrl: ""
-    }
+        profileUrl: searchParams.get("picture")
+    };
 
     const [ signupUser, setSignupUser ] = useState(user);
 
@@ -27,7 +29,7 @@ function SignUp(props) {
         try {
             await instance.post("/api/auth/sign-up", signupUser);
             alert("회원가입 완료");
-            window.location.replace("/auth/signin");
+            // window.location.replace("/auth/signin");
         }catch(error) {
             console.error(error);
             alert("회원가입 실패");
@@ -65,9 +67,9 @@ function SignUp(props) {
     return (
         <SignLayout>
             <h1>회원가입</h1>
-            <div css={S.inputBox}> <label>이메일</label> <input type="email" name='email' placeholder='이메일을 입력하세요' onChange={handleInputChange}/></div><button onClick={handleCheckEmailDuplicate}>중복 확인</button>
+            <div css={S.inputBox}> <label>이메일</label> <input type="email" name='email' placeholder='이메일을 입력하세요' value={signupUser?.email} onChange={handleInputChange}/></div><button onClick={handleCheckEmailDuplicate}>중복 확인</button>
             <div css={S.inputBox}> <label>비밀번호</label> <input type="password" name='password'  placeholder='비밀번호를 입력하세요' onChange={handleInputChange}/></div>
-            <div css={S.inputBox}> <label>이름</label> <input type="text" name='name' placeholder='이름을 입력하세요' onChange={handleInputChange}/></div>
+            <div css={S.inputBox}> <label>이름</label> <input type="text" name='name' placeholder='이름을 입력하세요' value={signupUser?.name} onChange={handleInputChange} disabled={true}/></div>
             <div css={S.inputBox}> <label>닉네임</label> <input type="text" name='nickname'  placeholder='닉네임을 입력하세요' onChange={handleInputChange}/></div>
             <div css={S.inputBox}> <label>전화번호</label> <input type="text" name='phone'  placeholder='전화번호를 입력하세요' onChange={handleInputChange}/></div>
             
